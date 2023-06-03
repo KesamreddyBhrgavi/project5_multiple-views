69a00606a9d73c1d75391be78187894853914f5e
kesamreddy Bhargavi <kesamreddybhargavi2@gmail.com>
2023-06-03 20:23:51 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def BHARGAVI(request):
+    return render(request,'BHARGAVI.html')
+def arpitha(request):
+    return render(request,'arpitha.html')
